389571b9035293ea212ffee934b2f925f0d93051
Ananta Pirdhaus (withPH) <ananta.firdaus2@gmail.com>
2023-10-12 11:24:48 +0700

    perubahan ketiga

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function add() {
+function Jumlahkan() {
   var num1 = parseInt(document.getElementById("num1").value);
   var num2 = parseInt(document.getElementById("num2").value);
-  var result = num1 + num2;
-  alert("Result: " + result);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("Isi Angka berikut dengan Benar");
+  } else {
+    var hasil = num1 + num2;
+    alert("Hasil penjumlahan: " + hasil);
+  }
+}
+
+function Ulang() {
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
